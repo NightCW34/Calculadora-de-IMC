7749abb5a89e912c547f788480056919cadae319
Arlen Asael <asaelmarencojaime5867@gmail.com>
2022-08-12 19:46:58 -0600

    Subida de ejercicio finalizado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Title } from "./Components";
+import { Container } from "./Components";
+import { Blank } from "./Components";
+import { Buttons } from "./Components";
+import { Result } from "./Components";
+import { Holder } from "./Components";
 
 export default function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [result, setResult] = useState(0);
+  const [imcColor, setColorImc] = useState<string>("");
+
+  const calculateImc = () => {
+    const imc = weight / (height * height);
+    const formattedImc = imc.toFixed(2);
+    setResult(+formattedImc);
+  };
+
+  const reset = () => {
+    setWeight(0);
+    setHeight(0);
+    setResult(0);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Title>Calculadora IMC ⚖️</Title>
+      <Holder>Peso (KG)</Holder>
+      <Blank
+        type="number"
+        onChange={(e) => setWeight(+e.target.value)}
+        value={weight}
+        placeholder="Peso (KG)"
+      />
+      <Holder>Altura (M)</Holder>
+      <Blank
+        type="number"
+        onChange={(e) => setHeight(+e.target.value)}
+        value={height}
+        placeholder="Altura (M)"
+      />
+
+     
+      {result > 0 && (
+              <Result>
+               <h2>Su imc es {result}</h2>
+               {0 < result && result < 18.5
+                 ? "Usted tiene un peso muy bajo, deberia subir de peso"
+                 : result < 24.5
+                 ? "Usted tiene un peso, normal siga asi"
+                 : result < 29.9
+                 ? "Usted tiene sobrepeso, deberia bajar de peso un poco"
+                 : result < 34.9
+                 ? "Usted tiene Obesidad de primer nivel, deberia bajar de peso"
+                 : result < 39.9
+                 ? "Usted tiene Obesidad de segundo nivel deberia bajar de peso"
+                 : result > 40 && "Usted tiene el tercer grado de Obesidad deberia bajar de peso urgentemente"}
+             </Result>
+      )}
+
+
+
+      <Buttons onClick={calculateImc}>Calcular</Buttons>
+      <Buttons onClick={reset}>Resetear</Buttons>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Components.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+
+font-size: 22px;
+text-align: center;
+font-family: sans-serif;
+margin-bottom: 20px;
+`
+export const Container = styled.section`
+
+width: 400px;
+background: #1bbc9b;
+padding: 35px;
+margin: auto;
+border-radius: 4px;
+font-family: sans-serif;
+color: #FFFFFF;
+`
+export const Blank = styled.input`
+ width 97.1%;
+ padding 3px;
+ items-align: center;
+ font-family: sans-serif;
+ border-radius: 4px;
+ margin-bottom: 16px;
+ border : none;
+ font-size: 18px;
+ placeholder: 
+`
+export const Buttons = styled.button`
+width 49%;
+background: #FFFFFF;
+border : none;
+padding : 12px;
+margin 1px;
+border-radius: 4px;
+
+&:hover {
+background: #2d3e50;
+color: #FFFFFF;
+transition: all 0.5s;
+}
+`
+export const Result = styled.h1`
+font-size: 15px;
+text-align: center;
+margin : 5px;
+width : 100%;
+`  
+export const Holder = styled.h1`
+font-size: 15px;
+color : #FFFFFF
+`
